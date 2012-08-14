031a0945c1d52e03b2fd3b7a4d4399d4aeadd4b7
Nick Weinhold <n.c.weinhold@googlemail.com>
2012-08-14 22:16:25 +0100

    Added a few more exercises from The Little Schemer. Included in this commit are firsts, insertR, insertL and subst.

--- littleschemer.rkt
+++ littleschemer.rkt
@@ -22,3 +22,30 @@
       ((null? lat) '())
       ((eq? (car lat) e) (cdr lat))
       (else (cons (car lat) (rember e (cdr lat)))))))
+
+(define firsts
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      (else (cons (car (car l)) (firsts (cdr l)))))))
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons old (cons new (cdr lat))))
+      (else (cons (car lat) (insertR new old (cdr lat)))))))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons new lat))
+      (else (cons (car lat) (insertL new old (cdr lat)))))))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) old) (cons new (cdr lat)))
+      (else (cons (car lat) (subst new old (cdr lat)))))))
